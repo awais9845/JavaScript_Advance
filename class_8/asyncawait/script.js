cf8d25bf74d6cba05db6cc70714a91b3f3f00b11
awais9845 <waqarkhattak844@gmail.com>
2026-02-10 08:35:53 -0800

    asyncawait|modules|classes studied today

---
+++ class_8/asyncawait/script.js
@@ -0,0 +1,44 @@
+// // let products = async()=>{
+// //     let response = await fetch(" https://api.escuelajs.co/api/v1/products");
+// //     let data= await response.json();
+// //     console.log(data);
+// // }
+// // products();
+
+
+// let products = async ()=>{
+//     try {
+//         let response = await fetch(" https://api.escuelajs.co/api/v1/products");
+//         let data =await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// }
+// products();
+
+
+
+let getProducts = async() =>{
+let newproducts = await fetch("https://fake-store-api.mock.beeceptor.com/api/produc");
+let data =await newproducts.json();
+console.log(data);
+}
+
+// getProducts();
+
+// let getProducts = async()=>{
+//     try {
+//         let newproducts = await fetch("https://fake-store-api.mock.beeceptor.com/api/produc");
+// let data =await newproducts.json();
+// console.log(data);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+getProducts();
+
+
+console.log("awaiskhan");
